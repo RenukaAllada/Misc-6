a6cc8cc6354efa6a19b6167fcc5b271c2d6312fd
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-04-20 13:11:22 -0500

    Completed 2 exercises

--- Sample.java
+++ Sample.java
@@ -26,4 +26,33 @@ class Sample{
     }
     /********************PROBLEM-2****************/
 
+    class Solution {
+        public int[][] multiply(int[][] mat1, int[][] mat2) {
+            if(mat1==null || mat2==null ||mat1.length==0 ||mat2.length==0){
+                return new int[][]{};
+            }
+
+            int m=mat1.length;
+            int n=mat1[0].length;
+            int p=mat2[0].length;
+
+            int[][] result=new int[m][p];
+
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(mat1[i][j]==0){
+                        continue;
+                    }
+                    for(int k=0;k<p;k++){
+                        if(mat2[j][k]==0){
+                            continue;
+                        }
+                        result[i][k]+=mat1[i][j] * mat2[j][k];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
 }
